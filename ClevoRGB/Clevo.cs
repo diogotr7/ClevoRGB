b69b447edfd6dd9092708f4dc9483e360a3c4aac
Diogo Trindade <diogotr7@gmail.com>
2019-12-12 13:34:12 +0000

    Initial Implementation

---
+++ ClevoRGB/Clevo.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace ClevoRGB
+{
+    public static class Clevo
+    {
+        #region Imports
+        [DllImport("perkey_api.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr InitPerkeyIo();
+
+        [DllImport("perkey_api.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void SetLEDStatus(byte cmd, byte data0, byte data1, byte data2, byte data3);
+
+        [DllImport("perkey_api.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern int GetLedData(byte cmd, byte type, byte[] arr, byte size);
+        #endregion
+
+        private static void SetKeyColor(byte key, byte r, byte g, byte b) => SetLEDStatus(1, key, r, g, b);
+
+        private const byte ROWS = 5;
+        private const byte COLUMNS = 19;
+
+        private static readonly byte[] _colors = new byte[384];
+        private static Dictionary<Key, int[]> _layout;
+
+        public static bool Initialize()
+        {
+            try
+            {
+                InitPerkeyIo();
+                _layout = Layouts.ISO15;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static void Update()
+        {
+            for (byte row = 0; row <= ROWS; row++)
+            {
+                for (byte col = 0; col <= COLUMNS; col++)
+                {
+                    var offset = GetIndex(row, col);
+                    byte r = _colors[offset + 0];
+                    byte g = _colors[offset + 1];
+                    byte b = _colors[offset + 2];
+
+                    byte key = Convert.ToByte(row << 5 | col);
+                    SetKeyColor(key, r, g, b);
+                }
+            }
+        }
+
+        public static void SetColorFull(Color clr)
+        {
+            for (byte row = 0; row <= ROWS; row++)
+            {
+                for (byte col = 0; col <= COLUMNS; col++)
+                {
+                    SetColorWithIndex(GetIndex(row, col), clr);
+                }
+            }
+        }
+
+        public static void SetKeyColor(Key key, Color clr)
+        {
+            if (_layout.TryGetValue(key, out var index))
+                for (byte i = 0; i < index.Length; i++)
+                    SetColorWithIndex(index[i], clr);
+            else
+                Console.WriteLine("failed to get value of" + key);
+        }
+
+        public static void SetCoordColor(byte row, byte col, Color clr) => SetColorWithIndex(GetIndex(row, col), clr);
+
+        private static void SetColorWithIndex(int idx, Color clr)
+        {
+            _colors[idx + 0] = clr.R;
+            _colors[idx + 1] = clr.G;
+            _colors[idx + 2] = clr.B;
+        }
+
+        private static int GetIndex(byte row, byte col)
+        {
+            return (64 * row) + (col * 3) + (col / 5);
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClevoRGB;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Clevo.Initialize())
+            {
+                Console.WriteLine("Initialized!");
+                Clevo.SetColorFull(Color.Blue);
+                Clevo.SetKeyColor(Key.ESCAPE, Color.Red);
+                Clevo.SetKeyColor(Key.W, Color.Red);
+                Clevo.SetKeyColor(Key.A, Color.Red);
+                Clevo.SetKeyColor(Key.S, Color.Red);
+                Clevo.SetKeyColor(Key.D, Color.Red);
+                Clevo.Update();
+            }
+            else
+            {
+                Console.WriteLine("NOT Initialized!");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
